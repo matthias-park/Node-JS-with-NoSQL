1ca63580f1575d5d9a68b86f089b22ca10c282a6
matthias <matthias@dfrc.kr>
2020-06-18 11:48:21 +0900

    2020.06.18 1. connected MongoDB

--- util/database.js
+++ util/database.js
@@ -1,9 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://matthias:bjs0220@node-complete-cqski.mongodb.net/<dbname>?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected!');
+            callback(client);
+        })
+        .catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect
 
