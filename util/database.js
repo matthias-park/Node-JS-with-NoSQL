d5f94d3a1df941960f2763d13a9e93d2f159879d
matthias <matthias@dfrc.kr>
2020-06-21 18:32:49 +0900

    2020.06.21 1.connect mongodb 2.fetch collection from mongodb

--- util/database.js
+++ util/database.js
@@ -1,14 +1,31 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://matthias:bjs0220@node-complete-cqski.mongodb.net/<dbname>?retryWrites=true&w=majority')
-        .then(client => {
-            console.log('Connected!');
-            callback(client);
-        })
-        .catch(err => console.log(err));
+    MongoClient.connect(
+        'mongodb+srv://matthias:<password>@node-complete-cqski.mongodb.net/shop?retryWrites=true&w=majority'
+        )
+            .then(client => {
+                console.log('Connected!');
+                _db = client.db();
+                callback();
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;   
+            });
 }
 
-module.exports = mongoConnect
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'no database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
 
