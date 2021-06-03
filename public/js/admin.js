d48ff1e5b586d2529e77305995c28735534346ac
matthias <matthiaspark0514@gmail.com>
2021-06-02 19:43:20 -0700

    add async requests

---
+++ public/js/admin.js
@@ -0,0 +1,23 @@
+const deleteProduct = (btn) => {
+  const prodId = btn.parentNode.querySelector("[name=productId]").value;
+  const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
+
+  const prodcutElement = btn.closest('article');
+
+  fetch("/admin/product/" + prodId, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrf,
+    },
+  })
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+      prodcutElement.parentNode.removeChild(prodcutElement);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
